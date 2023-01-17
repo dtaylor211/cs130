66c125efda653d0b957e1700d0c925f41f6d39cb
dtaylor <dtaylor@caltech.edu>
2023-01-17 00:50:45 -0800

    DJJ-8: Working on cell.py

--- sheets/cell.py
+++ sheets/cell.py
@@ -1,5 +1,8 @@
 class CellType:
-    pass
+    LITERAL: int = 1
+    STRING: int = 2
+    FORMULA: int = 3
+    EMPTY: int = 4
 
 
 class Cell:
@@ -7,28 +10,29 @@ class Cell:
     This class represents an individual cell in a spreadsheet.
 
     '''
-
+    
     def __init__(self):
         self.contents = None
         self.value = None
-        self.type
+        self.type: int = CellType.EMPTY
 
     def set_value(self, input_str: str):
         inp = input_str.strip()
         if inp != "":
             self.contents = inp
             if inp[0] == "'":
-                # want to set to StringType?
+                self.type = CellType.STRING
                 if len(inp) == 1:
                     self.value = ""
                 else:
                     self.value = inp[1:]
             elif inp[0] == "=":
-                pass
-                # todo
-                # evaluate contents to set value 
+                self.type = CellType.FORMULA
+                self.value = Formula(inp) #todo
+                pass # evaluate formula here, maybe formula class
         else: 
             self.contents = None
             self.value = None
+            self.type = CellType.EMPTY
 
 
