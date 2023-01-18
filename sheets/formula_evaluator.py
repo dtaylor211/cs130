6d36c3cb7d15fa3a15a60a6bade336b24281538e
dtaylor <dtaylor@caltech.edu>
2023-01-18 14:01:58 -0800

    DJJ-12: Now works with decimals, removes extra 0s

--- sheets/formula_evaluator.py
+++ sheets/formula_evaluator.py
@@ -5,23 +5,42 @@ parser = Lark.open('formulas.lark', start='formula', rel_to=__file__)
 
 class Evaluator(Transformer):
     '''
-    tbd
+    Evaluate an input string as a formula, based on the described Lark grammar
+    in ./formulas.lark
 
     '''
+
     def __init__(self):
+        # Initialize an evaluator, as well as the Transformer
+
         Transformer.__init__(self)
 
     def NUMBER(self, token: Token):
-        return Decimal(token)
+        # Evaluate a NUMBER type into a Decimal object   
+        #
+        # Make sure to normalize 
+        norm = Decimal(token).normalize()
+        _, _, exp = norm.as_tuple()
+        if exp <= 0:
+            return norm
+        else:
+            return norm.quantize(1)
     
     def STRING(self, token: Token):
+        # Evaluate a STRING type
+
         return str(token)[0][1:-1]
 
     def ERROR_VALUE(self, token: Token):
+        # Evaluate an ERROR_VALUE within the Tree
         pass
         # return CellError(token)
 
     def add_expr(self, args):
+        # Evaluate an addition expression within the Tree
+        #
+        # Right now this does not work for complex expressions
+
         operator = args[1]
         a = args[0].children[-1]
         b = args[2].children[-1]
@@ -31,6 +50,10 @@ class Evaluator(Transformer):
             return a - b
     
     def mul_expr(self, args):
+        # Evaluate a multiplication expression within the Tree
+        #
+        # Right now this does not work for complex expressions
+
         operator = args[1]
         a = args[0].children[-1]
         b = args[2].children[-1]
@@ -38,3 +61,12 @@ class Evaluator(Transformer):
             return a * b
         else:
             return a / b
+
+    def unary_op(self, args):
+        # Evaluate a unary operation within the Tree
+        # 
+        # Need to check if this works
+
+        operator = args[0]
+        a = args[1].children[-1]
+        return -1 * a if operator == '-' else a
