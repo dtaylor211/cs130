9c1036e04b690135738a47983e997e14ec80cf3c
Ben Juarez <bjuarez@caltech.edu>
2023-01-18 14:15:57 -0800

    DJJ-13: Added bare test methods (mostly)

---
+++ tests/test_sheet.py
@@ -0,0 +1,25 @@
+import pytest
+from sheets.sheet import Sheet
+
+class TestSheet:
+    '''
+    (Spread)Sheet tests (project 1)
+    '''
+
+    def test_extent(self):
+        pass
+
+    def test_get_cell(self):
+        pass
+
+    def test_get_coords_from_loc(self):
+        pass
+
+    def test_get_contents(self):
+        pass
+
+    def test_set_contents(self):
+        pass
+
+    def test_get_value(self):
+        pass
---
+++ tests/test_workbook.py
@@ -0,0 +1,40 @@
+import pytest
+from sheets.workbook import Workbook
+
+class TestWorkbook:
+    '''
+    Workbook tests (project 1)
+    '''
+
+    def test_empty_workbook(self):
+        pass
+
+    def test_new_sheet_generate_name(self):
+        pass
+
+    def test_new_sheet_valid_name(self):
+        pass
+
+    def test_new_sheet_invalid_name(self):
+        pass
+
+    def test_del_sheet(self):
+        pass
+
+    def test_extent(self):
+        pass
+
+    def test_reference(self):
+        pass
+
+    def test_dependency(self):
+        pass
+
+    def test_set_contents(self):
+        pass
+
+    def test_get_contents(self):
+        pass
+
+    def test_get_value(self):
+        pass
