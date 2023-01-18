40d175751db1a27212fc4f97dd10cb3ca42abe6c
dtaylor <dtaylor@caltech.edu>
2023-01-17 21:27:03 -0800

    DJJ-8: Fixed formatting

--- sheets/cell.py
+++ sheets/cell.py
@@ -1,42 +1,88 @@
-class CellType:
-    # Types of values contained in a cell
-    
-    LITERAL: int = 1
+import enum
+
+class CellType(enum.Enum):
+    '''
+    This enum specifies the kinds of values that spreadsheet cells can hold.
+    '''
+
+    NUMBER: int = 1
     STRING: int = 2
-    FORMULA: int = 3
+    FORMULA: int = 3 # May want to remove
     EMPTY: int = 4
+
+    # ERROR: int = 5 ?
 
 
 class Cell:
-    # A cell containing values of CellType and their string contents.
-    #  
-    # This class represents an individual cell in a spreadsheet.
+    '''
+    A cell containing values of CellType and their string contents.
+      
+    This class represents an individual cell in a spreadsheet.
+    Stores the string contents as well as the value
+    Stores the type of the value as a CellType
+
+    '''
     
-    def __init__(self):
+    def __init__(self, loc: str):
+        '''
+        Initialize a new Cell object
+
+        Arguments:
+        - loc: str - alphanumeric code representing location on a sheet (B2)
+
+        '''
+
+        self.loc = loc
+
+        # new Cell is treated as an empty cell, contents and values are None
         self.contents = None
         self.value = None
         self.type: int = CellType.EMPTY
 
     def set_value(self, input_str: str):
+        '''
+        Set the value of the cell.
+
+        Arguments:
+        - input_str: str - specifications to set new cell value
+
+        '''
+
+        # Remove leading and trailing whitespace
         inp = input_str.strip()
+    
+        # Check if empty string
         if inp != "":
+
             self.contents = inp
+
+            # Check if there is a leading single quote, set to STRING type
             if inp[0] == "'":
                 self.type = CellType.STRING
-                if len(inp) == 1:
-                    self.value = ""
-                else:
-                    self.value = inp[1:]
+                self.value = inp[1:]
+
+            # Check if there is a leading equal sign, set to FORMULA type
+            # and evaluate
             elif inp[0] == "=":
                 self.type = CellType.FORMULA
                 self.value = Formula(inp) #todo
                 pass # evaluate formula here, maybe formula class
+
+            # Otherwise set to LITERAL type
+            else:
+                self.type = CellType.NUMBER # want to do a better job figure out expression to match input  Lark?
+                self.value = inp # prob not what I want to do - maybe need string and int literals
         else: 
             self.contents = None
             self.value = None
             self.type = CellType.EMPTY
 
     def empty(self):
+        '''
+        Empty the contents of a cell
+
+        '''
+        
         self.contents = None
         self.value = None
         self.type: int = CellType.EMPTY
