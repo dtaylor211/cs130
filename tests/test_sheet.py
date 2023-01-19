c304ce0657d3bbaeab8c896d50345e49a8c0405e
Ben Juarez <bjuarez@caltech.edu>
2023-01-18 23:51:20 -0800

    DJJ-13: Improved Sheet tests

--- tests/test_sheet.py
+++ tests/test_sheet.py
@@ -25,6 +25,9 @@ class TestSheet:
 
     def test_get_coords_from_loc(self):
         sheet = Sheet("Sheet1")
+        with pytest.raises(ValueError):
+            sheet.get_coords_from_loc("A0") # check more cases?
+
         (col, row) = sheet.get_coords_from_loc("A1")
         assert (col, row) == (1, 1)
 
@@ -37,20 +40,22 @@ class TestSheet:
         (col, row) = sheet.get_coords_from_loc("AAC750")
         assert (col, row) == (705, 750)
 
-        # check more
-
     # not applicable?
     # def test_get_cell(self):
     #     pass
 
     def test_get_contents(self):
-        # TODO
-        pass
+        sheet = Sheet("Sheet1")
+        assert sheet.set_cell_contents("A1") is None
 
-    def test_set_contents(self):
-        # TODO
-        pass
+        sheet.set_cell_contents("A1", "1")
+        contents = sheet.get_cell_contents("A1")
+        assert contents == "1"
 
     def test_get_value(self):
-        # TODO
-        pass
+        sheet = Sheet("Sheet1")
+        assert sheet.set_cell_contents("A1") is None
+
+        sheet.set_cell_contents("A1", "1")
+        value = sheet.get_cell_value("A1")
+        assert value == Decimal(1)
