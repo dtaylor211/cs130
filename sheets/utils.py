ea07fee14b9cf7a5079923920f1ddd749732790c
dtaylor <dtaylor@caltech.edu>
2023-02-01 14:15:47 -0800

    DJJ-38: Adding error explanations to comments

--- sheets/utils.py
+++ sheets/utils.py
@@ -1,6 +1,19 @@
 from typing import Tuple
 
 def get_loc_from_coords(coords: Tuple[int, int]) -> str:
+    '''
+    Get a cell location from its coordinates
+
+    Throw a ValueError if the coordinates are invalid or out of bounds
+
+    Arguments:
+    - coords: Tuple[int, int] - tuple of col, row coordinates
+
+    Returns:
+    - str of cell location
+
+    '''
+    
     col, row = coords
     if col < 1 or row < 1 or col > 9999 or row > 9999:
         raise ValueError("Invalid coordinates")
