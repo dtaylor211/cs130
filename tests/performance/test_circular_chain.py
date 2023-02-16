e087e8c09dfcd376740843735a82074676b45637
dtaylor <dtaylor@caltech.edu>
2023-02-16 14:50:49 -0800

    DJJ-75: Added/linted performance tests for move/copy

--- tests/performance/test_circular_chain.py
+++ tests/performance/test_circular_chain.py
@@ -1,36 +1,51 @@
+'''
+Performance - Test Circular Chain
+
+Performance tests for when a large circular cell reference chain is created
+in the workbook.
+
+When running this file, a profile is output to the terminal with the 10
+most time consuming parts of the computation listed.
+
+Methods:
+- test_circular_chain() -> None
+
+'''
+
 import cProfile
+from pstats import Stats
+
+# pylint: disable=unused-import, import-error
+from sheets import Workbook, CellError, CellErrorType
 from tests import context
 
-from pstats import Stats
-from decimal import Decimal
-from sheets import *
 
-def test_circular_chain():
+def test_circular_chain() -> None:
     '''
     Stress test for a long chain of references that produces a cycle
 
     '''
 
-    wb = Workbook()
-    (index, name) = wb.new_sheet('Sheet1')
+    wb1 = Workbook()
+    _, name = wb1.new_sheet('Sheet1')
 
-    wb.set_cell_contents(name, 'A1', '=1')
+    wb1.set_cell_contents(name, 'A1', '=1')
 
     for i in range(2, 801):
-        wb.set_cell_contents(name, f'A{i}', f'=A{i - 1}')
+        wb1.set_cell_contents(name, f'A{i}', f'=A{i - 1}')
 
-    wb.set_cell_contents(name, 'A1', '=A800')
-    value = wb.get_cell_value(name, 'A1')
+    wb1.set_cell_contents(name, 'A1', '=A800')
+    value = wb1.get_cell_value(name, 'A1')
     assert isinstance(value, CellError)
     assert value.get_type() == CellErrorType.CIRCULAR_REFERENCE
-    value = wb.get_cell_value(name, 'A800')
+    value = wb1.get_cell_value(name, 'A800')
     assert isinstance(value, CellError)
     assert value.get_type() == CellErrorType.CIRCULAR_REFERENCE
 
 if __name__ == '__main__':
     profiler = cProfile.Profile()
     profiler.enable()
-    
+
     test_circular_chain()
 
     profiler.disable()
--- tests/performance/test_rename_volume2.py
+++ tests/performance/test_rename_volume2.py
@@ -1,39 +1,61 @@
-import cProfile
-from tests import context
+'''
+Performance - Test Rename Volume 2
 
+Performance tests for when a large volume of cells in a sheet that 
+references itself and that sheet is renamed.  
+
+For a similar test that involves a reference chain in another sheet, 
+see test_rename_chain.py.
+For a similar test that involves a large volume of references in another
+sheet, see test_rename_volume1.py
+
+When running this file, a profile is output to the terminal with the 10
+most time consuming parts of the computation listed.
+
+Methods:
+- test_rename_volume2() -> None
+
+'''
+
+
+import cProfile
 from pstats import Stats
 from decimal import Decimal
-from sheets import *
+
+# pylint: disable=unused-import, import-error
+from sheets import Workbook
+from tests import context
+
 
 def test_rename_volume2():
     '''
-    Stress tests for renaming a sheet when it causes a reference chain of cells in its own sheet to be updated
+    Stress tests for renaming a sheet when it causes a reference chain
+    of cells in its own sheet to be updated
 
     '''
-    
-    wb = Workbook()
-    _, name = wb.new_sheet('Sheet1')
 
-    wb.set_cell_contents(name, 'A1', '1')
-    value = wb.get_cell_value(name, 'A1')
+    wb1 = Workbook()
+    _, name = wb1.new_sheet('Sheet1')
+
+    wb1.set_cell_contents(name, 'A1', '1')
+    value = wb1.get_cell_value(name, 'A1')
 
     for i in range(2, 801):
-        wb.set_cell_contents(name, f'A{i}', f'={name}!A{i-1}')
+        wb1.set_cell_contents(name, f'A{i}', f'={name}!A{i-1}')
 
-    # wb.set_cell_contents(name, 'A801', f'={name}!A1')
-    wb.rename_sheet(name, name+'1')
-    value = wb.get_cell_value(name+'1', 'A1')
+    wb1.rename_sheet(name, name+'1')
+    value = wb1.get_cell_value(name+'1', 'A1')
     assert value == Decimal('1')
-    
-    contents = wb.get_cell_contents(name+'1', 'A800')
-    value = wb.get_cell_value(name+'1', 'A800')
+
+    contents = wb1.get_cell_contents(name+'1', 'A800')
+    value = wb1.get_cell_value(name+'1', 'A800')
     assert contents == f'={name}1!A799'
     assert value == Decimal('1')
 
 if __name__ == '__main__':
     profiler = cProfile.Profile()
     profiler.enable()
-    
+
     test_rename_volume2()
 
     profiler.disable()
